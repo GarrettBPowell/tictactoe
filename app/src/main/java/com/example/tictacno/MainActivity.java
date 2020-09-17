ce45be61f3fa3315e3ab1a79e36fd227f6dbf7f8
Garrett Powell <garettbpowell@gmail.com>
2020-09-16 19:09:38 -0500

    Base game working except for timed reset

--- app/src/main/java/com/example/tictacno/MainActivity.java
+++ app/src/main/java/com/example/tictacno/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.tictacno;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button[][] buttons = new Button[3][3];
+    private ImageButton[][] buttons = new ImageButton[3][3];
 
     private boolean player1Turn = true;
 
@@ -29,12 +33,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textViewPlayer1 = findViewById(R.id.text_view_p1);
         textViewPlayer2 = findViewById(R.id.text_view_p2);
 
+
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 String buttonID = "button_" + i + j;
                 int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
                 buttons[i][j] = findViewById(resID);
                 buttons[i][j].setOnClickListener(this);
+                buttons[i][j].setBackgroundResource(R.drawable.trash);
+                buttons[i][j].setTag("trash");
             }
         }
 
@@ -49,14 +56,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        if (!((Button) v).getText().toString().equals("")) {
+        if (!((ImageButton) v).getTag().equals("trash")){
             return;
         }
 
         if (player1Turn) {
-            ((Button) v).setText("X");
-        } else {
-            ((Button) v).setText("O");
+            ((ImageButton) v).setBackgroundResource(R.drawable.screaming);
+            ((ImageButton) v).setTag("scream");
+        }
+        else {
+            ((ImageButton) v).setBackgroundResource(R.drawable.racoon);
+            ((ImageButton) v).setTag("rac");
         }
 
         roundCount++;
@@ -76,18 +86,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private boolean checkForWin() {
-        String[][] field = new String[3][3];
+        Object[][] field = new Object[3][3];
 
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                field[i][j] = buttons[i][j].getText().toString();
+                field[i][j] = buttons[i][j].getTag();
             }
         }
 
         for (int i = 0; i < 3; i++) {
             if (field[i][0].equals(field[i][1])
                     && field[i][0].equals(field[i][2])
-                    && !field[i][0].equals("")) {
+                    && !field[i][0].equals("trash")) {
                 return true;
             }
         }
@@ -95,20 +105,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         for (int i = 0; i < 3; i++) {
             if (field[0][i].equals(field[1][i])
                     && field[0][i].equals(field[2][i])
-                    && !field[0][i].equals("")) {
+                    && !field[0][i].equals("trash")) {
                 return true;
             }
         }
 
         if (field[0][0].equals(field[1][1])
                 && field[0][0].equals(field[2][2])
-                && !field[0][0].equals("")) {
+                && !field[0][0].equals("trash")) {
             return true;
         }
 
         if (field[0][2].equals(field[1][1])
                 && field[0][2].equals(field[2][0])
-                && !field[0][2].equals("")) {
+                && !field[0][2].equals("trash")) {
             return true;
         }
 
@@ -142,7 +152,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void resetBoard() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                buttons[i][j].setText("");
+                buttons[i][j].setBackgroundResource(R.drawable.trash);
+                buttons[i][j].setTag("trash");
             }
         }
 
