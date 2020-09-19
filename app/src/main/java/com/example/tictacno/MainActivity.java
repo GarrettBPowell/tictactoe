dd403d88949f32a28d507b646e89b1146d4cf90e
Garrett Powell <garettbpowell@gmail.com>
2020-09-19 16:29:22 -0500

    SOUNDS

--- app/src/main/java/com/example/tictacno/MainActivity.java
+++ app/src/main/java/com/example/tictacno/MainActivity.java
@@ -2,12 +2,12 @@ package com.example.tictacno;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textViewPlayer1 = findViewById(R.id.text_view_p1);
         textViewPlayer2 = findViewById(R.id.text_view_p2);
 
-
+//set all tags to "trast" and all background images to the trashcan
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 String buttonID = "button_" + i + j;
@@ -54,12 +54,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
     }
 
+//use tags to check the state of the image button
     @Override
     public void onClick(View v) {
         if (!((ImageButton) v).getTag().equals("trash")){
             return;
         }
-
         if (player1Turn) {
             ((ImageButton) v).setBackgroundResource(R.drawable.screaming);
             ((ImageButton) v).setTag("scream");
@@ -71,15 +71,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         roundCount++;
 
-        if (checkForWin()) {
-            if (player1Turn) {
+        if (checkForWin())
+        {
+            if (player1Turn)
+            {
                 player1Wins();
-            } else {
+            }
+            else
+            {
                 player2Wins();
             }
-        } else if (roundCount == 9) {
+        }
+        else if (roundCount == 9)
+        {
             draw();
-        } else {
+        }
+        else
+        {
             player1Turn = !player1Turn;
         }
 
@@ -88,33 +96,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private boolean checkForWin() {
         Object[][] field = new Object[3][3];
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
                 field[i][j] = buttons[i][j].getTag();
-            }
         }
 
-        for (int i = 0; i < 3; i++) {
-            if (field[i][0].equals(field[i][1])
-                    && field[i][0].equals(field[i][2])
-                    && !field[i][0].equals("trash")) {
+        for (int i = 0; i < 3; i++)
+        {
+            if (field[i][0].equals(field[i][1]) && field[i][0].equals(field[i][2]) && !field[i][0].equals("trash"))
                 return true;
-            }
         }
 
-        for (int i = 0; i < 3; i++) {
-            if (field[0][i].equals(field[1][i])
-                    && field[0][i].equals(field[2][i])
-                    && !field[0][i].equals("trash")) {
+        for (int i = 0; i < 3; i++)
+        {
+            if (field[0][i].equals(field[1][i]) && field[0][i].equals(field[2][i]) && !field[0][i].equals("trash"))
                 return true;
-            }
         }
 
-        if (field[0][0].equals(field[1][1])
-                && field[0][0].equals(field[2][2])
-                && !field[0][0].equals("trash")) {
+        if (field[0][0].equals(field[1][1]) && field[0][0].equals(field[2][2]) && !field[0][0].equals("trash"))
             return true;
-        }
 
         if (field[0][2].equals(field[1][1])
                 && field[0][2].equals(field[2][0])
@@ -125,23 +126,52 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return false;
     }
 
-    private void player1Wins() {
+    private void player1Wins()
+    {
+        MediaPlayer yosh = MediaPlayer.create(this, R.raw.yoshi);
+        yosh.start();
         player1Points++;
-        Toast.makeText(this, "Player 1 wins!", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Opossum wins!", Toast.LENGTH_SHORT).show();
         updatePointsText();
-        resetBoard();
+    //delay reset board
+        new Handler().postDelayed(new Runnable()
+        {
+            public void run () {
+                resetBoard();
+            }
+        }, 2000);
     }
 
-    private void player2Wins() {
+    private void player2Wins()
+    {
+       MediaPlayer yosh = MediaPlayer.create(this, R.raw.yoshi);
+       yosh.start();
         player2Points++;
-        Toast.makeText(this, "Player 2 wins!", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Raccoon wins!", Toast.LENGTH_SHORT).show();
         updatePointsText();
-        resetBoard();
+
+    //delay reset board
+        new Handler().postDelayed(new Runnable()
+        {
+            public void run ()
+            {
+                resetBoard();
+            }
+        }, 2000);
     }
 
-    private void draw() {
+    private void draw()
+    {
+        MediaPlayer ahh = MediaPlayer.create(this, R.raw.ahh);
+        ahh.start();
         Toast.makeText(this, "Draw!", Toast.LENGTH_SHORT).show();
-        resetBoard();
+
+        //delay reset board
+        new Handler().postDelayed(new Runnable() {
+            public void run () {
+                resetBoard();
+            }
+        }, 1000);
     }
 
     private void updatePointsText() {
